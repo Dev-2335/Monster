7ddd6e2116d16aa6136a6d61334d8205d2688940
Rutvik Dangar <112462822+RutvikDangar@users.noreply.github.com>
2024-07-04 18:40:16 +0530

    fonts

--- monster/src/Pages/Home.jsx
+++ monster/src/Pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-// import  green_energy_font from 'Assets/Fonts/GREEN_ENERGY.ttf';
+import  green_energy_font from '../Assets/Fonts/GREEN_ENERGY.ttf';
 
 const Home = () => {
   return (
-    // <div style={{fontStyle:green_energy_font}}>
-        <div>
-      "Home Page"
+    <div style={{fontFamily:'green_energy_font'}}>
+      Home Page
     </div>
   )
 }
