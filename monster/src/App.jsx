b2f90e3b5c1b1c79ce9ba1a8a0102aba0dac36f5
Rutvik Dangar <112462822+RutvikDangar@users.noreply.github.com>
2024-07-06 20:16:00 +0530

    Background Animation Complete.

--- monster/src/App.jsx
+++ monster/src/App.jsx
@@ -2,10 +2,39 @@ import React from 'react';
 import BG from './Components/BG';
 
 
+const bgStyle ={
+  position: 'fixed',
+  top:0,
+  left:0,
+  zIndex: -25
+}
+
+
+const mainStyle={
+  position:'absolute',
+  top:0,
+  left:0,
+  zIndex: 100,
+  color:'white',
+}
+
+const Main = () => {
+  return (
+    <div>
+      <h1>HIi</h1>
+    </div>
+  )
+}
+
 const App = () => {
   return (
     <>
+    <div >
     <BG/>
+    </div>
+    <div style={mainStyle}>
+      <Main/>
+    </div>
     </>
   );
 };
